898ec947d07593beb5fd7053a96d51ca7cce9790
xuanbao95 <bao.lgx@gmail.com>
2021-01-20 20:27:34 +0700

    ầ

--- js_lan2/danhsachsinhvien.js
+++ js_lan2/danhsachsinhvien.js
@@ -9,8 +9,9 @@ function DanhSachSinhVien() {
             if (this.arr[i].maSV === maSV) {
                 viTri = i;
                 break;
-            }
-        }
+            };
+
+        };
         return viTri;
     };
     this.xoaSinhVien = function (maSV) {
@@ -20,39 +21,29 @@ function DanhSachSinhVien() {
                 break;
             };
         };
+
     };
     this.layThongTinSinhVien = function (maSV) {
         var viTri = this.timViTri(maSV);
-        if (viTri !== -1) {
-            //tìm thấy sinh viên
-            return this.arr[viTri];
-        }
-
-    };
-    this.capNhatSinhVien = function (sv) {
-        var viTri = this.timViTri(sv.maSV);
-        if (viTri !== -1) {
-            return this.arr[viTri] = sv;
-        }
-    };
-    //tìm kiếm sinh viên
-    this.timKiemSinhVien = function (keyword) {
-        /**
-         * 0. tao mangTimKiem = [];
-         * 1. Duyệt mảng arr
-         * 2. Nếu như keyword trùng với object.tenSV
-         *  => mangTimKiem thêm sinh viên dc tìm thấy
-         * 3. trả về mangTimKiem
-         */
-        var mangTimKiem = [];
         for (var i = 0; i < this.arr.length; i++) {
-            if (
-                this.arr[i].tenSV.toLowerCase().indexOf(keyword.toLowerCase()) !== -1
-            ) {
-                mangTimKiem.push(this.arr[i]);
+            if (viTri !== -1) {
+                return this.arr[viTri];
             }
         }
-
+    };
+    this.capNhat = function (SV) {
+        var viTri = this.timViTri(SV.maSV);
+        if (viTri !== -1) {
+            return this.arr[viTri] = SV;
+        };
+    };
+    this.timKiem = function (keyWord) {
+        var mangTimKiem = [];
+        for (var i = 0; i < this.arr.length; i++) {
+            if (this.arr[i].tenSV.toLowerCase().indexOf(keyWord.toLowerCase()) !== -1) {
+                mangTimKiem.push(this.arr[i]);
+            };
+        };
         return mangTimKiem;
     };
 };
--- js_lan2/main.js
+++ js_lan2/main.js
@@ -15,88 +15,82 @@ function layThongTinNguoiNhap() {
     var _diemHoa = getEle("txtDiemHoa").value;
     var isvalid = true;
     if (_maSV === "") {
-        getEle("spanMaSV").innerHTML = "vui long nhập";
+        getEle("spanMaSV").innerHTML = "vui lòng nhập";
         isvalid = false;
     } else {
         getEle("spanMaSV").innerHTML = "";
         isvalid = true;
-    };
+    }
     if (isvalid = true) {
         var sinhVien = new SinhVien(_maSV, _tenSV, _email, _pass, _date, _khoaHoc, _diemToan, _diemLy, _diemHoa);
         return sinhVien;
     };
     return null;
-
-
 };
 function themSinhVien(event) {
     event.preventDefault();
     var sinhVien = layThongTinNguoiNhap();
     sinhVien.tinhDTB();
     dssv.themSinhVien(sinhVien);
-    taobang(dssv.arr);
+    taoBang(dssv.arr);
     setLocalStorage();
-    console.log(sinhVien);
+
 };
-//tạo bảng
-function taobang(arr) {
-    var content = "";
-    for (var i = 0; i < arr.length; i++) {
-        content += `
-        <tr>
-        <td>${arr[i].maSV}</td>
-        <td>${arr[i].tenSV}</td>
-        <td>${arr[i].email}</td>
-        <td>${arr[i].date}</td>
-        <td>${arr[i].khoaHoc}</td>
-        <td>${arr[i].diemTB}</td>
-        <td>
-            <button class="btn btn-danger" onclick="xoaSinhVien('${arr[i].maSV}')">xóa</button>
-            <button class="btn btn-info" onclick="suaSinhVien('${arr[i].maSV}')">sửa</button>
-        </td>
-        </tr>
-    `;
-    }
-    getEle("tbodySinhVien").innerHTML = content;
-};
-//xóa sinh viên
 function xoaSinhVien(maSV) {
     dssv.xoaSinhVien(maSV);
-    taobang(dssv.arr);
+    taoBang(dssv.arr);
     setLocalStorage();
 };
-//sữa sinh viên
 function suaSinhVien(maSV) {
-    getEle("btnCapNhat").style.display = "inline-block";
-    var laySinhVien = dssv.layThongTinSinhVien(maSV);
-    getEle("txtMaSV").value = laySinhVien.maSV;
+    var sinhVien = dssv.layThongTinSinhVien(maSV);
+    getEle("btnCapNhat").style.display = "inline";
+    getEle("txtMaSV").value = sinhVien.maSV;
     getEle("txtMaSV").disabled = true;
-    getEle("txtTenSV").value = laySinhVien.tenSV;
-    getEle("txtEmail").value = laySinhVien.email;
-    getEle("txtPass").value = laySinhVien.pass;
-    getEle("txtNgaySinh").value = laySinhVien.date;
+    getEle("txtTenSV").value = sinhVien.tenSV;
+    getEle("txtEmail").value = sinhVien.email;
+    getEle("txtPass").value = sinhVien.pass;
+    getEle("txtNgaySinh").value = sinhVien.date;
+    getEle("khSV").value = sinhVien.khoaHoc;
+    getEle("txtTenSV").value = sinhVien.tenSV;
 };
-//cập nhật sau khi sửa
 getEle("btnCapNhat").addEventListener("click", function () {
     var sinhVien = layThongTinNguoiNhap();
     sinhVien.tinhDTB();
-    dssv.capNhatSinhVien(sinhVien);
-    taobang(dssv.arr);
+    dssv.capNhat(sinhVien)
+    taoBang(dssv.arr);
     setLocalStorage();
 });
-//reset
 function resetForm() {
-    getEle("formSV").reset();
+    getEle("formSV").value = "";
     getEle("btnCapNhat").style.display = "none";
     getEle("txtMaSV").disabled = false;
-}
-//tìm kiếm
+};
 getEle("txtSearch").addEventListener("keyup", function () {
     var keyWord = getEle("txtSearch").value;
-    var mangTimKiem = dssv.timKiemSinhVien(keyWord);
-    taobang(mangTimKiem);
-})
+    var mangTimKiem = dssv.timKiem(keyWord);
+    taoBang(mangTimKiem);
+});
+function taoBang(arr) {
+    var content = "";
+    for (var i = 0; i < arr.length; i++) {
+        content += `
+            <tr>
+                <td>${arr[i].maSV}</td>
+                <td>${arr[i].tenSV}</td>
+                <td>${arr[i].email}</td>
+                <td>${arr[i].date}</td>
+                <td>${arr[i].khoaHoc}</td>
+                <td>${arr[i].diemTB}</td>
+                <td>
+                    <button class="btn btn-danger" onclick="xoaSinhVien('${arr[i].maSV}')">xoa</button>  
+                    <button class="btn btn-info" onclick="suaSinhVien('${arr[i].maSV}')">sua</button>  
 
+                </td>
+            </tr>
+        `
+    };
+    getEle("tbodySinhVien").innerHTML = content;
+}
 function setLocalStorage() {
     var arrString = JSON.stringify(dssv.arr);
     localStorage.setItem("DSSV", arrString);
@@ -104,6 +98,6 @@ function setLocalStorage() {
 function getLocalStorage() {
     if (localStorage.getItem("DSSV")) {
         dssv.arr = JSON.parse(localStorage.getItem("DSSV"));
-        taobang(dssv.arr);
+        taoBang(dssv.arr);
     };
 };
